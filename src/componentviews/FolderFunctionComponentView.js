ff0f4f2e8bb45d7d6c4d0d8f106af328b9cd2bef
sutter-dave <sutter@intransix.com>
2021-08-11 17:20:45 -0700

    work on property refinement

--- src/componentviews/FolderFunctionComponentView.js
+++ src/componentviews/FolderFunctionComponentView.js
@@ -49,37 +49,26 @@ FolderFunctionComponentView.ICON_RES_PATH = "/icons3/pageFunctionIcon.png";
 FolderFunctionComponentView.TREE_ENTRY_SORT_ORDER = ParentComponentView.FOLDER_COMPONENT_TYPE_SORT_ORDER;
 
 
-FolderFunctionComponentView.propertyDialogLines = [
+FolderFunctionComponentView.propertyDialogEntries = [
     {
-        "type":"textField",
-        "label":"Arg List: ",
-        "size": 80,
-        "key":"argListString"
+        propertyKey: "argList",
+        dialogElement: {
+            "type":"textField",
+            "label":"Arg List: ",
+            "size": 80,
+            "key":"argListString"
+        },
+        propertyToForm: argListValue => argListValue.toString(),
+        formToProperty: argListString => apogeeutil.parseStringArray(argListString)
     },
     {
-        "type":"textField",
-        "label":"Return Val: ",
-        "size": 40,
-        "key":"returnValue"
+        propertyKey: "returnValue",
+        dialogElement: {
+            "type":"textField",
+            "label":"Return Val: ",
+            "size": 40,
+            "key":"returnValue"
+        }
     }
 ];
-/** This function takes the proeprty values from the component/members and converts it to have the format for the dialog form.
- * This function may be omitted if no transformation is needed. If it is included, the proeprty values can be
- * written into. */
-FolderFunctionComponentView.propertyToFormValues = propertyValues => {
-    propertyValues.argListString = propertyValues.argList.toString();
-    return propertyValues;
-}
-/** This function takes the result value of the form and converts it to have the format for property values.
- * This function may be omitted if no transformation is needed. If it is included, the form values can be
- * written into. */
-FolderFunctionComponentView.formToPropertyValues = formValue => {
-    formValue.argList = apogeeutil.parseStringArray(formValue.argListString);
-    return formValue
-}
-/** This function indicates any properties that are sourced by proeprty dialog lines with a different
- * key. This map can be completely omitted if all the property keys match the dialog keys. Also, and
- * proeprty key which matches a dialog key can be omitted. */
-FolderFunctionComponentView.dialogLineMap = {
-    argList: "argListString"
-}
+
