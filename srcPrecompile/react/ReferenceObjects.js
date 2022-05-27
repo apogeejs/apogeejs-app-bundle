52ede17b9b1160490798685b03ce07bb23cd36e5
sutter-dave <sutter@intransix.com>
2022-05-27 16:57:44 -0700

    added reference tree entries

--- srcPrecompile/react/ReferenceObjects.js
+++ srcPrecompile/react/ReferenceObjects.js
@@ -1,18 +1,19 @@
 import {TreeEntry} from "./TreeView.js"
+import {referenceViewConfigList} from "/apogeejs-app-bundle/src/referenceViewConfig.js"
 import {uiutil} from "/apogeejs-ui-lib/src/apogeeUiLib.js"
 
 
-/** This file contains the functions to generate the tree objects for the workspace. */
+/** This file contains the functions to generate the tree objects for the references. */
 
-export function ReferencesTreeEntry({referenceManager, openTab}) {
+export function ReferencesTreeEntry({referenceManager}) {
 
-    const childTreeEntries = [
-    ]
+    const childTreeEntries = referenceViewConfigList.map(referenceViewConfig => 
+        <ReferenceListTreeEntry key={referenceViewConfig.referenceType} referenceManager={referenceManager} 
+            referenceViewConfig={referenceViewConfig} />)
 
     var iconUrl = uiutil.getResourcePath(REFERENCES_ICON_PATH,"app");
 
     return <TreeEntry 
-        key={referenceManager.getId()}
         iconSrc={iconUrl} 
         text={REFERENCES_TREE_LABEL} 
         status={referenceManager.getStatus()} 
@@ -21,3 +22,52 @@ export function ReferencesTreeEntry({referenceManager, openTab}) {
 
 const REFERENCES_ICON_PATH = "/icons3/folderIcon.png";
 const REFERENCES_TREE_LABEL = "Libraries"
+
+
+function ReferenceListTreeEntry({referenceManager, referenceViewConfig}) {
+
+    const app = referenceManager.getApp()
+
+    const menuItems = referenceViewConfig.listMenuItems.map(menuItem => {
+        return {
+                text: menuItem.text,
+                action: () => menuItem.callback(app)
+            }
+        }
+    )
+
+    let entryDataList = referenceManager.getModuleList(referenceViewConfig.referenceType)
+    const childTreeEntries = entryDataList.map(entryData => { 
+        let referenceEntry = referenceManager.getRefEntryById(entryData.entryId)
+        return <ReferenceEntryTreeEntry key={referenceEntry.getId()} app={app} referenceEntry={referenceEntry} referenceViewConfig={referenceViewConfig} />
+    })
+
+    var iconUrl = uiutil.getResourcePath(referenceViewConfig.listIconPath,"app");
+
+    return <TreeEntry 
+        iconSrc={iconUrl} 
+        text={referenceViewConfig.listName} 
+        menuItems={menuItems} 
+        status={"normal"} 
+        childTreeEntries={childTreeEntries} />
+}
+
+function ReferenceEntryTreeEntry({app,referenceEntry, referenceViewConfig}) {
+
+    const menuItems = referenceViewConfig.entryMenuItems.map(menuItem => {
+        return {
+                text: menuItem.text,
+                action: () => menuItem.callback(app,referenceEntry)
+            }
+        }
+    )
+
+    var iconUrl = uiutil.getResourcePath(referenceViewConfig.entryIconPath,"app");
+
+    return <TreeEntry 
+        key={referenceViewConfig.referenceType}
+        iconSrc={iconUrl} 
+        text={referenceEntry.getDisplayName()} 
+        menuItems={menuItems} 
+        status={"normal"} />
+}
--- srcPrecompile/referenceViewConfig.js
+++ srcPrecompile/referenceViewConfig.js
@@ -1,27 +1,23 @@
 import WebApogeeModuleEntryViewConfig from "/apogeejs-app-bundle/src/references/WebApogeeModuleEntryViewConfig.js";
-import NodeApogeeModuleEntryViewConfig from "/apogeejs-app-bundle/src/references/NodeApogeeModuleEntryViewConfig.js";
+//import NodeApogeeModuleEntryViewConfig from "/apogeejs-app-bundle/src/references/NodeApogeeModuleEntryViewConfig.js";
 import EsModuleEntryViewConfig from "/apogeejs-app-bundle/src/references/EsModuleEntryViewConfig.js";
-import NpmModuleEntryViewConfig from "/apogeejs-app-bundle/src/references/NpmModuleEntryViewConfig.js";
+//import NpmModuleEntryViewConfig from "/apogeejs-app-bundle/src/references/NpmModuleEntryViewConfig.js";
 import JsScriptEntryViewConfig from "/apogeejs-app-bundle/src/references/JsScriptEntryViewConfig.js";
 import CssEntryViewConfig from "/apogeejs-app-bundle/src/references/CssEntryViewConfig.js";
 
-import ReferenceView from "/apogeejs-app-bundle/src/references/ReferenceView.js";
+export const referenceViewConfigList = [
+    WebApogeeModuleEntryViewConfig,
+    EsModuleEntryViewConfig,
+    JsScriptEntryViewConfig,
+    CssEntryViewConfig
+]
 
-/** This file initializes the reference class types available. */
 
-let referenceViewConfigMap = {};
-if(__APOGEE_ENVIRONMENT__ == "WEB") {
-    referenceViewConfigMap[WebApogeeModuleEntryViewConfig.referenceType] = WebApogeeModuleEntryViewConfig;
-    referenceViewConfigMap[EsModuleEntryViewConfig.referenceType] = EsModuleEntryViewConfig;
-    referenceViewConfigMap[JsScriptEntryViewConfig.referenceType] = JsScriptEntryViewConfig;
-    referenceViewConfigMap[CssEntryViewConfig.referenceType] = CssEntryViewConfig;
-}
-else if(__APOGEE_ENVIRONMENT__ == "NODE") {
-    referenceViewConfigMap[NodeApogeeModuleEntryViewConfig.referenceType] = NodeApogeeModuleEntryViewConfig;
-    referenceViewConfigMap[NpmModuleEntryViewConfig.referenceType] = NpmModuleEntryViewConfig;
-}
-else {
-    console.log("Warning - apogee environment not recognized!");
-}
+if(__APOGEE_ENVIRONMENT__ == "NODE") throw new Error("References not configured for Node environment!")
 
-ReferenceView.setReferenceViewConfigMap(referenceViewConfigMap);
+// const referenceViewConfigList = [
+//     NodeApogeeModuleEntryViewConfig,
+//     NpmModuleEntryViewConfig
+// ]
+
+
